96f5502d04271087a2307348f2eb2c037b58c816
mj-han <tarot0312@gmail.com>
2022-02-23 15:38:54 +0900

    공지사항, faq, 스토어 추가

--- pc/js/common.js
+++ pc/js/common.js
@@ -66,7 +66,6 @@ jQuery(function () {
       $(this).addClass('is-active');
       $('.main-search__top').hide(); // 버튼클릭시 검색결과 텍스트 숨기기
 
-      console.log($(this).find('a').text());
       return false;
     }
 
@@ -175,9 +174,6 @@ jQuery(function () {
       }
 
       $('.product-option-fix').css($styleOpt);
-      console.group('================');
-      console.log('scY : ', $sc);
-      console.groupEnd();
       return;
     });
   } // 주문서 배송지 option fix scroll
@@ -226,7 +222,7 @@ jQuery(function () {
         //li
     tab = '[class $= -tab__fix]',
         //ul
-    contents = $('.detail-tab__info'),
+    contents = $('[class *= -tab__info]'),
         //tab content
     idx = $(this).parent().index();
     $(this).closest(tab).children().removeClass('is-current');
@@ -255,7 +251,7 @@ jQuery(function () {
         //li
     tab = '[class $= -tab__fix]',
         //ul
-    contents = $('.payment-section-tab__info'),
+    contents = $('[class *= -tab__info]'),
         //tab content
     idx = $(this).parent().index();
     $(this).closest(tab).children().removeClass('is-current');
@@ -274,7 +270,41 @@ jQuery(function () {
     return false;
   }
 
-  $(document).on('click', '.js-payment-open', paymentTab); //--END[탭] ----------------------
+  $(document).on('click', '.js-payment-open', paymentTab); // 고객센터 > 멤버쉽 탭
+
+  function membershipTab() {
+    var item = '[class $= __item]',
+        //li
+    tab = '[class $= -tab__fix]',
+        //ul
+    contents = $('[class *= -tab__info]'),
+        //tab content
+    idx = $(this).parent().index();
+    $(this).closest(tab).children().removeClass('is-current');
+    $(this).parent(item).addClass('is-current');
+    contents.removeClass('is-current');
+    contents.eq(idx).addClass('is-current');
+    return false;
+  }
+
+  $(document).on('click', '.js-membership-tab', membershipTab); //스토어 탭
+
+  function storesTab() {
+    var item = '[class $= __item]',
+        //li
+    tab = '[class $= -tab__fix]',
+        //ul
+    contents = $('[class *= -tab__info]'),
+        //tab content
+    idx = $(this).parent().index();
+    $(this).closest(tab).children().removeClass('is-current');
+    $(this).parent(item).addClass('is-current');
+    contents.removeClass('is-current');
+    contents.eq(idx).addClass('is-current');
+    return false;
+  }
+
+  $(document).on('click', '.js-stores-tab', storesTab); //--END[탭] ----------------------
 
   /*---------------------
   * [select] :: custom 
@@ -302,7 +332,7 @@ jQuery(function () {
     function selectedTextChange(e) {
       var link = e.target,
           value = link.innerText,
-          select = link.parentNode.parentNode;
+          select = link.parentNode.parentNode; //ul
 
       if (link.parentNode.className === optName.replace('.', '')) {
         //hidden input 에 value 값 넣기
@@ -460,7 +490,21 @@ jQuery(function () {
     return false;
   }
 
-  $(document).on('click', '.js-qna-more', qnaMore); //--END[dropdown::아코디언] ----------------------
+  $(document).on('click', '.js-qna-more', qnaMore); //faq 더보기(아코디언)
+
+  function faqMore() {
+    var parent = $(this).closest('.notice-faq__item');
+
+    if (parent.hasClass('is-active')) {
+      parent.removeClass('is-active');
+    } else {
+      parent.addClass('is-active').siblings('li').removeClass('is-active');
+    }
+
+    return false;
+  }
+
+  $(document).on('click', '.js-faq-more', faqMore); //--END[dropdown::아코디언] ----------------------
 
   /*---------------------
     * [swiper slider] 
@@ -640,6 +684,25 @@ jQuery(function () {
       },
       effect: "fade",
       loop: true
+    });
+  } //스토어 스페셜 슬라이드
+
+
+  if ($('.special-slide').length > 0) {
+    var specialSlide = new Swiper('.special-slide__container', {
+      observer: true,
+      observeParents: true,
+      watchOverflow: true,
+      loop: true,
+      slidesPerView: 1,
+      pagination: {
+        el: ".special-slide__pagination",
+        type: "fraction"
+      },
+      navigation: {
+        nextEl: ".special-slide__next",
+        prevEl: ".special-slide__prev"
+      }
     });
   } //--END[swiper slider]-----------------------------
 
