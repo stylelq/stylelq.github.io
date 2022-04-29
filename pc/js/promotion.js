e6b68706ed7930f0bb2e897cb917d26858d04345
saebomlee <lsbbom@gmail.com>
2022-04-29 14:08:34 +0900

    가정의달 프로모션 영상 소리 추가 수정중

--- pc/js/promotion.js
+++ pc/js/promotion.js
@@ -97,3 +97,14 @@ if ($('.promotion-thumb').length > 0 && $('.promotion-thumb__item').length > 1) 
     promotionSlide.slideTo(num);
   });
 }
+
+if ($('.promotion-view__video--src').length > 0) {
+  var video = document.querySelector('.promotion-view__video--src');
+  var videoMuted = video.muted;
+
+  if (videoMuted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+}
