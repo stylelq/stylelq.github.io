56ce39dd681bd2137b83935a91811968f346c394
saebomlee <lsbbom@gmail.com>
2022-08-05 16:20:12 +0900

    fyl3 텍스트 애니메이션 수정

--- mobile/js/fyl.js
+++ mobile/js/fyl.js
@@ -87,14 +87,8 @@ function showTextYellow(divId, count, textLength, time) {
       showTextYellow(divId, count, textLength, time);
     }, time);
   }
-}
+} // 범위 랜덤함수(소수점 2자리까지)
 
-$(document).ready(function () {
-  var text1 = "Shape.Shifter 쉐이프 쉬프터는 신화나 소설 속에서 형태가 변신하는 능력자를 의미하며 이야기 속에서 물리적인 모양을 변신하는 능력은 초인간적인 능력, 신의 개입, 악마의 조작, 마법과 주문 같은 초월의 능력으로 가능했습니다. 역사적으로 누적된 수많은 설화와 사례들은 고정된 모습을 전복하려는 인간의 모양 변신에 대한 욕망을 투사합니다. Find Your Light 3는 루이까또즈의 헤리티지인 가방이 가지고 있는 원형의 한계를 뛰어 넘어 가방에 물리적 변형을 가하며 자유롭게 창작하는 프로젝트입니다. 10인의 아티스트와 함께 패션과 미술의 혼성적 경계에서 새로운 차원의 감각을 추구하며 동시대 예술의 가능성을 탐험합니다.";
-  textProcess('#contentText1', text1);
-  var text2 = "양승빈은 역사적으로 누적된 수많은 설화와 사례들은 고정된 모습을 전복하려는 인간의 모양  변신에 대한 욕망을 투사합니다.  올해 Find your light3는 루이까또즈의 헤리티지인 가방이 가지고 있는 원헤리티지인 가방이 가지고 있는 원헤리티지인 가방이 가지고 있는 원형의 한계를 뛰어 넘어 가방에 물리적 변형을 가하며 자유롭게 창작하는 프로젝트입니다. <br><br>양승빈은 역사적으로 누적된 수많은 설화와 사례들은 고정된 모습을 전복하려는 인간의 모양  변신에 대한 욕망을 투사합니다.  올해 Fi헤리티지인 가방이 가지고 있는 원헤리티지인 가방이 가지고 있는 원nd your light3는 루이까또즈의 헤리티지인 가방이 가지고 있는 원형의 한계를 뛰어 넘어 가방에 물리적 변형을 가하며 자유롭게 창작하는 프로젝트입니다.";
-  textYellow('#contentText2', text2);
-}); // 범위 랜덤함수(소수점 2자리까지)
 
 function random(min, max) {
   return parseFloat((Math.random() * (max - min) + min).toFixed(2));
@@ -125,3 +119,36 @@ floatingEvent('.floating7', 6, 15, 15);
 floatingEvent('.floating8', 6, -15, 15);
 floatingEvent('.floating9', 6, 15, 15);
 floatingEvent('.floating10', 6, -15, 15);
+/*function fylPlayVideo(){
+    var areaElem = $(this);
+    var areaNum = areaElem.attr('class').replace(/[^0-9]/g, '') - 1;
+    areaElem.find('.fyl-section1__intro').addClass('is-hide');
+    videoNum = areaNum;
+    objAry[videoNum].playVideo();
+}*/
+
+var playBtn = $('.fyl-section1__play');
+var pauseBtn = $('.fyl-section1__pause');
+var video = $('#stopVideo');
+
+function fylPauseVideo() {
+  playBtn.removeClass('is-hide');
+  video.get(0).pause();
+}
+
+function fylReplayVideo() {
+  video.get(0).play();
+  playBtn.addClass('is-hide');
+} //$('.js-fyl-btn').on('click', fylPlayVideo); // 영상재생
+
+
+$('#stopVideo').on('click', fylPauseVideo); // 영상정지
+
+$('#replayVideo').on('click', fylReplayVideo); //영상재실행
+
+$(document).ready(function () {
+  var text1 = "<b>Shape.Shifter 쉐이프 쉬프터</b>는 신화나 소설 속에서<br><b>형태가 변신하는 능력자</b>를 의미하며 이야기 속에서<br><b>물리적인 모양을 변신하는 능력</b>은 초인간적인 능력,<br>신의 개입, 악마의 조작, 마법과 주문 같은 초월의<br>능력으로 가능했습니다.<br>역사적으로 누적된 수많은 설화와 사례들은 고정된<br>모습을 전복하려는 인간의 모양 변신에 대한 욕망을<br>투사합니다. Find Your Light 3는 루이까또즈의<br>헤리티지인 가방이 가지고 있는 원형의 한계를 뛰어<br>넘어 가방에 물리적 변형을 가하며 자유롭게<br>창작하는 프로젝트입니다.<br>10인의 아티스트와 함께 패션과 미술의<br>혼성적 경계에서 새로운 차원의 감각을 추구하며<br>동시대 예술의 가능성을 탐험합니다.";
+  textProcess('#contentText1', text1);
+  var text2 = "양승빈은 역사적으로 누적된 수많은 설화와 사례들은 고정된 모습을 전복하려는 인간의 모양  변신에 대한 욕망을 투사합니다.  올해 Find your light3는 루이까또즈의 헤리티지인 가방이 가지고 있는 원헤리티지인 가방이 가지고 있는 원헤리티지인 가방이 가지고 있는 원형의 한계를 뛰어 넘어 가방에 물리적 변형을 가하며 자유롭게 창작하는 프로젝트입니다. <br><br>양승빈은 역사적으로 누적된 수많은 설화와 사례들은 고정된 모습을 전복하려는 인간의 모양  변신에 대한 욕망을 투사합니다.  올해 Fi헤리티지인 가방이 가지고 있는 원헤리티지인 가방이 가지고 있는 원nd your light3는 루이까또즈의 헤리티지인 가방이 가지고 있는 원형의 한계를 뛰어 넘어 가방에 물리적 변형을 가하며 자유롭게 창작하는 프로젝트입니다.";
+  textYellow('#contentText2', text2);
+});
