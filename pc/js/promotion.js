27949808db1d3ae202f2fb4fb915ce0dd4636acc
saebomlee <lsbbom@gmail.com>
2022-08-25 11:45:48 +0900

    fyl3 - 양승빈 작가페이지 이미지 교체, 하단 버튼영역 수정중

--- pc/js/promotion.js
+++ pc/js/promotion.js
@@ -106,34 +106,25 @@ if ($('.promotion-view__video--src').length > 0) {
     video.muted = false;
   } else {
     video.muted = true;
-    ㅓ;
   }
 } // 2022 9월 프로모션
-
-
-function openCouponPopup(e) {
-  // 쿠폰열기
-  e.preventDefault();
-  $('.thanksgiving-dim').addClass('is-show');
-  $('.thanksgiving-popup').addClass('is-show');
-}
-
-$('.js-coupon-open').on('click', openCouponPopup);
-
-function openCloseButton(e) {
-  // 닫기버튼 열기
-  e.preventDefault();
-  $('.thanksgiving-pop__button').addClass('is-colorChange');
-  $('.thanksgiving-pop__close').addClass('is-show');
-  $('.thanksgiving-pop__link').text('쿠폰 발급 완료!');
-}
-
-$('.js-coupon-download').on('click', openCloseButton);
-
-function closePopup(e) {
-  // 팝업닫기
-  $('.thanksgiving-dim').removeClass('is-show');
-  $('.thanksgiving-popup').removeClass('is-show');
-}
-
-$('.js-popup-close').on('click', closePopup);
+// function openCouponPopup(e){
+//     e.preventDefault();
+//     $('.thanksgiving-dim').addClass('is-show');
+//     $('.thanksgiving-popup').addClass('is-show');
+// }
+// $('.js-coupon-open').on('click', openCouponPopup);
+//
+// function openCloseButton(e){
+//     e.preventDefault();
+//     $('.thanksgiving-pop__button').addClass('is-colorChange');
+//     $('.thanksgiving-pop__close').addClass('is-show');
+//     $('.thanksgiving-pop__link').text('쿠폰 발급 완료!');
+// }
+// $('.js-coupon-download').on('click', openCloseButton);
+//
+// function closePopup(e){ // 팝업닫기
+//     $('.thanksgiving-dim').removeClass('is-show');
+//     $('.thanksgiving-popup').removeClass('is-show');
+// }
+// $('.js-popup-close').on('click', closePopup);
