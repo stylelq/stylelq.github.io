c9bb425f23ce36697513d26bfe9067db5f74a287
saebomlee <lsbbom@gmail.com>
2022-11-11 10:52:27 +0900

    [모바일]미니캐비어 기획전 퍼블

--- mobile/js/promotion.js
+++ mobile/js/promotion.js
@@ -69,3 +69,163 @@ if ($('.promotion-thumb').length > 0 && $('.promotion-thumb__item').length > 1) 
 //     $('.thanksgiving-popup').removeClass('is-show');
 // }
 // $('.js-popup-close').on('click', closePopup);
+// 2022.11 minicavir
+
+
+var mouse = {
+  x: 0,
+  y: 0
+};
+var showcasePos = 0;
+var showcaseCount = 20;
+var cardGap = 100;
+var cardWidth = 215;
+var padding = 20;
+var touch = false;
+var mobile = false;
+var pageReady = true;
+var current = '';
+var root = '';
+
+function listeners() {
+  var loadCount = 0;
+  $(".showcase-cards .card img").one('load', function () {
+    loadCount++;
+    console.log(loadCount);
+
+    if (loadCount == $(".showcase-cards .card").length) {
+      pageHandler('showcase');
+    }
+  }).each(function () {
+    if (this.complete) {
+      $(this).trigger('load');
+    }
+  });
+  $(".card--new").click(function () {
+    if (pageReady) {
+      pageReady = false;
+
+      if ($(".card--wrapper .card").length > 0) {
+        $(".card--options").removeClass('active');
+        $(".button--main").removeClass('active');
+        clearCard();
+      }
+    }
+  });
+  $("body").mousemove(function (e) {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+  });
+}
+
+function showcaseOffset() {
+  if (!touch) {
+    $(".showcase-cards .card").each(function (i) {
+      var index = showcaseCount - 1 - i;
+      var offset = cardGap * index;
+      $(this).css({
+        'left': offset + 'px'
+      });
+    });
+  }
+}
+
+function pageHandler(dest) {
+  if (dest != current) {
+    pageReady = false;
+    current = dest;
+
+    if (dest == 'showcase') {
+      $(".showcase").addClass('active');
+      setTimeout(function () {
+        pageReady = true;
+      }, 500);
+    }
+  }
+}
+
+function textareaHeight(element) {
+  element.style.height = "5px";
+  element.style.height = element.scrollHeight + "px";
+}
+
+function clearCard() {
+  $(".button--submit").removeClass('active');
+  var card = $(".card--wrapper .card");
+  card.addClass('remove');
+  setTimeout(function () {
+    card.remove();
+  }, 1000);
+}
+
+function anim() {
+  requestAnimationFrame(anim);
+
+  if (!touch) {
+    var pos = mouse.x / window.innerWidth * 1.4 - 0.2;
+
+    if (pos > 1) {
+      pos = 1;
+    }
+
+    if (pos < 0) {
+      pos = 0;
+    }
+
+    var index = Math.round(pos * (showcaseCount - 1));
+    $(".showcase-cards .card").each(function (i) {
+      if (showcaseCount - 1 - i < index) {
+        $(this).addClass('push');
+      } else {
+        $(this).removeClass('push');
+      }
+    });
+    var edge = 100;
+
+    if (mobile) {
+      edge = 30;
+    }
+
+    var g = (cardGap * (showcaseCount - 1) + cardWidth - window.innerWidth + padding * 2 + edge * 2) / (showcaseCount - 1);
+    var offset = edge - index * g;
+    showcasePos += (offset - showcasePos) / 10;
+    $(".showcase-cards").css({
+      'transform': 'translate(' + showcasePos + 'px, 0px)'
+    });
+  } else {
+    var pos = $(".showcase").scrollLeft() / ($(".showcase-cards").outerWidth() - window.innerWidth) * 1 - 0;
+
+    if (pos > 1) {
+      pos = 1;
+    }
+
+    if (pos < 0) {
+      pos = 0;
+    }
+
+    var index = Math.round(pos * (showcaseCount - 1));
+    $(".showcase-cards .card").each(function (i) {
+      if (showcaseCount - 1 - i < index) {
+        $(this).addClass('push');
+      } else {
+        $(this).removeClass('push');
+      }
+    });
+  }
+}
+
+$(document).ready(function () {
+  if ("ontouchstart" in document.documentElement) {
+    touch = true;
+    $("body").addClass('touch');
+  }
+
+  if (showcaseCount > $(".showcase-cards .card").length) {
+    showcaseCount = $(".showcase-cards .card").length;
+  }
+
+  listeners();
+  showcaseOffset();
+  requestAnimationFrame(anim);
+  $(window).resize(resizeHandler);
+});
